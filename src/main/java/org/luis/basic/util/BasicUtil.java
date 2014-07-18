fd4c463705c15a6edf17b6584d84903978ae6461
Li, Guoliang <kxmd2008@gmail.com>
2014-07-18 17:22:00 +0800

    update

---
+++ src/main/java/org/luis/basic/util/BasicUtil.java
@@ -0,0 +1,19 @@
+package org.luis.basic.util;
+
+import java.io.IOException;
+
+public class BasicUtil {
+	
+	public static String getWebAppPath() {
+		String path = "";
+		try {
+			// 通过Spring ApplicationContext获取资源绝对路径
+			path = SpringContextAware.getApplicationContext().getResource("")
+					.getFile().getAbsolutePath()
+					+ "/";
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return path;
+	}
+}
