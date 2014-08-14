bad24f9513fd206308bec8a8a033051bd7ec197c
kxmd2008 <kxmd2008@gmail.com>
2014-08-14 13:03:53 +0800

    d

--- src/main/java/org/luis/basic/util/BasicUtil.java
+++ src/main/java/org/luis/basic/util/BasicUtil.java
@@ -19,4 +19,5 @@ public class BasicUtil {
 		}
 		return path;
 	}
+	
 }
--- src/main/java/org/luis/basic/util/StringUtils.java
+++ src/main/java/org/luis/basic/util/StringUtils.java
@@ -1,5 +1,6 @@
 package org.luis.basic.util;
 
+
 public class StringUtils {
 	
 	public static boolean isNullOrBlank(String str){
@@ -13,43 +14,8 @@ public class StringUtils {
 	}
 	
 	public static String[] split(String src, String del) {
-		if (src == null)
-			return null;
-
-		if (del == null) {
-			String[] res = new String[1];
-			res[0] = src;
-			return (res);
-		}
-		if (src.equals("")) {
-			String[] res = new String[1];
-			res[0] = src;
-			return (res);
-		}
-
-		String[] res = new String[src.length()];
-		String src1 = new String(src);
-		int i = 0, j = 0;
-
-		for (; !src1.equals("");) {
-			i = src1.indexOf(del);
-			if (i == -1)
-				break;
-			res[j++] = src1.substring(0, i);
-			src1 = src1.substring(i + del.length());
-		}
-
-		if (!src1.equals(""))
-			res[j] = src1;
-
-		// shrink the string array
-		String[] ret = new String[j + 1];
-		for (i = 0; i <= j; i++) {
-			ret[i] = res[i];
-			res[i] = null;
-		}
-
-		res = null;
-		return ret;
+		return org.springframework.util.StringUtils.split(src, del);
 	}
+	
+	
 }
