81f06547dc74a749b2fdbf5776fc0878e586e36e
kxmd2008 <kxmd2008@gmail.com>
2014-08-26 14:50:27 +0800

    update

--- src/main/java/org/luis/basic/rest/model/SimpleMessage.java
+++ src/main/java/org/luis/basic/rest/model/SimpleMessage.java
@@ -2,48 +2,25 @@ package org.luis.basic.rest.model;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 /**
- * 简单业务请求消息对象,可以转化成为Json数据格式,主要是定位于Rpc访问/MQTT消息传递等 参见《NGBF_移动框架接口说明》
- * 
- * 
+ * 简单业务请求消息对象,可以转化成为Json数据格式
  */
 public class SimpleMessage<T> implements java.io.Serializable {
 
-	/**
-	 * 
-	 */
 	public static final long serialVersionUID = 6693914877246416586L;
 
 	/**
 	 * 消息头对象
-	 * 
-	 * 
 	 */
-	private SimpleMessageHead head = new SimpleMessageHead();
+	private SimpleMessageHead head;
 
 	/**
-	 * 消息单数据体，用于实现单数据 改为<String,Object>
-	 * 
-	 * 
-	 */
-	private Map<String, Object> data = null;
-	/**
-	 * 消息多数据体，用于实现多数据 改为<String,Object>
-	 * 
-	 * 
-	 */
-	private List<Map<String, Object>> items = null;
-	/**
 	 * 对象数据模型,放置对象
-	 * 
-	 * 
 	 */
 	private List<T> records = null;
 
@@ -51,8 +28,6 @@ public class SimpleMessage<T> implements java.io.Serializable {
 
 	/**
 	 * 添加条目对象
-	 * 
-	 * 
 	 * @param record
 	 * @return
 	 */
@@ -66,8 +41,6 @@ public class SimpleMessage<T> implements java.io.Serializable {
 
 	/**
 	 * 获得所有条目对象
-	 * 
-	 * 
 	 */
 	public List<T> getRecords() {
 		if (records == null) {
@@ -77,48 +50,15 @@ public class SimpleMessage<T> implements java.io.Serializable {
 	}
 
 	/**
-	 * 添加Item数据对象 改为<String,Object>
-	 * 
-	 */
-	public SimpleMessage<T> add(Map<String, Object> item) {
-		if (items == null) {
-			items = new ArrayList<Map<String, Object>>();
-		}
-		items.add(item);
-		return this;
-	}
-
-	/**
-	 * 获得Item数据列表对象
-	 * 
-	 * 
-	 */
-	public List<Map<String, Object>> getItems() {
-		return items;
-	}
-
-	/**
-	 * 构造函数，指定消息头和指定业务数据Map
-	 * 
-	 * 
-	 */
-	public SimpleMessage(SimpleMessageHead head, Map<String, Object> data) {
-		this.head = head;
-		this.data = data;
-	}
-
-	/**
 	 * 空构造函数
 	 * 
 	 */
 	public SimpleMessage() {
-		this.head = new SimpleMessageHead("");
+		this.head = new SimpleMessageHead();
 	}
 
 	/**
 	 * 链式API:指定空消息头
-	 * 
-	 * 
 	 */
 	public static SimpleMessage<?> blank() {
 		return new SimpleMessage<Object>();
@@ -126,8 +66,6 @@ public class SimpleMessage<T> implements java.io.Serializable {
 
 	/**
 	 * 链式API:指定SimpleMessageHead的消息头
-	 * 
-	 * 
 	 */
 	public SimpleMessage<?> head(SimpleMessageHead head) {
 		this.head = head;
@@ -136,26 +74,13 @@ public class SimpleMessage<T> implements java.io.Serializable {
 
 	/**
 	 * 构造函数，指定消息头
-	 * 
-	 * 
 	 */
 	public SimpleMessage(SimpleMessageHead head) {
 		this.head = head;
 	}
 
 	/**
-	 * 构造函数,指定apiName作为消息头
-	 * 
-	 * @deprecated 参见blank().head()
-	 */
-	public SimpleMessage(String api_code) {
-		this.head = new SimpleMessageHead(api_code);
-	}
-
-	/**
 	 * 将对象生成Json对象数据
-	 * 
-	 * 
 	 */
 	public String toJson() {
 		Gson gson = new Gson();
@@ -176,64 +101,7 @@ public class SimpleMessage<T> implements java.io.Serializable {
 	}
 
 	/**
-	 * 设置单数据
-	 * 
-	 * 
-	 */
-	public SimpleMessage<T> set(Map<String, Object> data) {
-		this.data = data;
-		return this;
-	}
-
-	/**
-	 * 设置单数据
-	 * 
-	 * 
-	 */
-	public SimpleMessage<T> setStringMap(Map<String, String> data) {
-		return this;
-	}
-
-	/**
-	 * 设置单数据值
-	 * 
-	 * 
-	 */
-	public SimpleMessage<T> set(String key, Object value) {
-		if (data == null) {
-			data = new HashMap<String, Object>();
-		}
-		data.put(key, value);
-		return this;
-	}
-
-	/**
-	 * 获得单数据值
-	 * 
-	 * 
-	 */
-	public String getData(String key) {
-		Object v = this.data.get(key);
-		if (v == null) {
-			v = "";
-		}
-		return v.toString();
-	}
-
-	/**
-	 * 获得整体的Data单数据集
-	 * 
-	 * 
-	 * @return
-	 */
-	public Map<String, Object> getData() {
-		return this.data;
-	}
-
-	/**
 	 * 获得Head
-	 * 
-	 * 
 	 */
 	public SimpleMessageHead getHead() {
 		return head;
@@ -241,8 +109,6 @@ public class SimpleMessage<T> implements java.io.Serializable {
 
 	/**
 	 * 设置Head
-	 * 
-	 * 
 	 */
 	public void setHead(SimpleMessageHead head) {
 		this.head = head;
@@ -254,6 +120,10 @@ public class SimpleMessage<T> implements java.io.Serializable {
 
 	public void setItem(T item) {
 		this.item = item;
+	}
+
+	public void setRecords(List<T> records) {
+		this.records = records;
 	}
 
 }
